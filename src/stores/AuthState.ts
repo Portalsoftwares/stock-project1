1a2a59723235ce398f1520b9cc02e457a25bc7f1
Makara OP <makara@cas.com.kh>
2024-01-10 10:46:53 +0700

    Update AuthTS

--- src/stores/AuthState.ts
+++ src/stores/AuthState.ts
@@ -4,15 +4,27 @@ import { defineStore } from 'pinia'
 
 export const useAuthStore  = defineStore('auth', () => {
 
-  const storedIsLogin = localStorage.getItem('isLogin')??false;
-  const storedToken  = localStorage.getItem('token')??'';
-  const storedUser = localStorage.getItem('user')??'';
-  const storedRole = localStorage.getItem('role')??'';
+  // const storedIsLogin = localStorage.getItem('isLogin')??false;
+  // const storedToken  = localStorage.getItem('token')??'';
+  // const storedUser = localStorage.getItem('user')??'';
+  // const storedRole = localStorage.getItem('role')??'';
 
-  const isLogin =  ref(JSON.parse(storedToken) ?? false);
-  const token = ref(JSON.parse(storedToken) ?? '');
-  const user = ref(JSON.parse(storedUser) ?? '');
-  const role = ref(JSON.parse(storedRole) ?? '');
+  // const isLogin =  ref(JSON.parse(storedToken) ?? false);
+  // const token = ref(JSON.parse(storedToken) ?? '');
+  // const user = ref(JSON.parse(storedUser) ?? '');
+  // const role = ref(JSON.parse(storedRole) ?? '');
+
+  // Retrieve values from localStorage with fallbacks
+  const storedIsLogin: boolean = localStorage.getItem('isLogin') === 'true';
+  const storedToken: string = localStorage.getItem('token') ?? '';
+  const storedUser: string = localStorage.getItem('user') ?? '';
+  const storedRole: string = localStorage.getItem('role') ?? '';
+
+  // Create refs with explicit types
+  const isLogin = ref<boolean>(storedIsLogin);
+  const token = ref<string>(JSON.parse(storedToken) ?? '');
+  const user = ref(JSON.parse(storedUser) ?? {});
+  const role = ref<string>(JSON.parse(storedRole) ?? '');
 
   const Authorization = computed(() =>   'Bearer ' + token.value  )
 
