4120ffdcfb881907e86aa0f174f5b97870fb45f5
Makara OP <makara@cas.com.kh>
2024-01-10 09:56:59 +0700

    Update VSales - Export -Non Tax -State Charge

--- src/stores/AuthState.ts
+++ src/stores/AuthState.ts
@@ -4,10 +4,10 @@ import { defineStore } from 'pinia'
 
 export const useAuthStore  = defineStore('auth', () => {
 
-  const isLogin =  ref(JSON.parse(localStorage.getItem('isLogin')) || false);
-  const token = ref(JSON.parse(localStorage.getItem('token')) || '');
-  const user = ref(JSON.parse(localStorage.getItem('user')) || []);
-  const role = ref(JSON.parse(localStorage.getItem('role')) || []);
+  const isLogin =  ref(JSON.parse(localStorage.getItem('isLogin')??'') || false);
+  const token = ref(JSON.parse(localStorage.getItem('token')??'') || '');
+  const user = ref(JSON.parse(localStorage.getItem('user')??'') || []);
+  const role = ref(JSON.parse(localStorage.getItem('role')??'') || []);
 
   const Authorization = computed(() =>   'Bearer ' + token.value  )
 
